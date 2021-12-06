d9e243725586a54c6f0794fb99ab885e7a49a663
李通洲 <carter.li@eoitek.com>
2021-12-06 16:43:25 +0800

    Remove outdated download_libs

--- other/download_libs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-location="https://github.com/jesec/homebrew-mpv-iina/releases/latest/download"
-IFS=$'\n' read -r -d '' -a files < <(curl -L "${location}/filelist.txt" && printf '\0')
-rm -rf deps/lib
-mkdir -p deps/lib
-for file in "${files[@]}"
-do
-  curl -L "${location}/${file}" -o deps/lib/$file
-done
