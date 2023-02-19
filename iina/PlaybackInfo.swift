fa244012fdf2898ad4b8a86073671ef5b62f79b1
low-batt <86170219+low-batt@users.noreply.github.com>
2023-02-19 16:39:33 -0500

    Fix fullscreen only works for first video, #4202
    This commit will:
    - Remove a check in `PlayerCore.fileLoaded` that only put the player
      into full screen mode for the first file
    - Remove the property `justLaunched` from `PlaybackInfo`
    
    This causes the "Enter fullscreen" setting under the "When media is
    opened" settings to when enabled put the player into full screen mode
    for all videos, not just the first video played.

--- iina/PlaybackInfo.swift
+++ iina/PlaybackInfo.swift
@@ -91,7 +91,6 @@ class PlaybackInfo {
     }
   }
 
-  var justLaunched: Bool = true
   var justStartedFile: Bool = false
   var justOpenedFile: Bool = false
   var shouldAutoLoadFiles: Bool = false
